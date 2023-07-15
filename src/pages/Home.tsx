a631dfa55855e125164868a2552ed661aaedc569
Edmir Galvan <edmirgalvaz@gmail.com>
2023-07-15 14:49:49 -0600

    Change heading element

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,9 +5,9 @@ export const Home = () => {
   return (
     <section className="Home">
       <h1>Lista de lectura</h1>
-      <h2>
+      <h3>
         8 libros disponibles <small>(2 en la lista de lectura)</small>
-      </h2>
+      </h3>
       <div className="Filters">
         <label>
           Filtrar por titulo
