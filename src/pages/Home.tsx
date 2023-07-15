843ba74dc00cf6ad585cb741abf256cf5fb78ee9
Edmir Galvan <edmirgalvaz@gmail.com>
2023-07-15 14:20:39 -0600

    HTML Home stucture

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,6 +5,25 @@ export const Home = () => {
   return (
     <section className="Home">
       <h1>Lista de lectura</h1>
+      <div className="Filters">
+        <label>
+          Filtrar por titulo
+          <input type="text" name="search" id="search" />
+        </label>
+        <label>
+          Filtrar por paginas
+          <input type="range" name="pages" id="pages" min="0" max="100" />
+        </label>
+        <label>
+          Filtrar por genero
+          <select name="genres" id="genres">
+            <option selected>Todas</option>
+            <option value="Fantasía">Fantasía</option>
+            <option value="Ciencia ficción">Ciencia ficción</option>
+            <option value="Terror">Terror</option>
+          </select>
+        </label>
+      </div>
       <ul>
         {books.library.map(({ book }) => (
           <li>{book.title}</li>
